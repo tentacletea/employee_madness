42e505adb9883e4754b458fb3379f92b6f6f8cbb
tentacletea <chen.lily@outlook.de>
2023-01-18 14:39:54 +0100

    a

--- server/routes/equipments.router.js
+++ server/routes/equipments.router.js
@@ -7,6 +7,9 @@ equipmentRouter.get("/", async (req, res) => {
       created: "desc"
     })
     return res.json(characters)
+    // res.send(characters)
+    // res.setHeader("Content-Type", "application/json")
+    // res.end(JSON.stringify(characters))
   })
 
 module.exports = equipmentRouter;
